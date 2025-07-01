dccc512fe96559354880de71f7cf9e175b405e93
zeks07 <76873691+zeks07@users.noreply.github.com>
2025-07-01 12:22:19 +0200

    exit process when done

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,18 @@ extern "C" void trap_vector_base();
 
 auto init() -> void {
   auto trap_vector_base_pointer = reinterpret_cast<uint64>(&trap_vector_base) | DIRECT_MODE;
-  __asm__ volatile("csrw stvec, %0" : : "r"(trap_vector_base_pointer));
+  __asm__ volatile ("csrw stvec, %0" : : "r"(trap_vector_base_pointer));
   MemoryAllocator::init();
+}
+
+auto end() -> void {
+  __asm__ volatile ("li t0, 0x5555");
+  __asm__ volatile ("li t1, 0x100000");
+  __asm__ volatile ("sw t0, 0(t1)");
 }
 
 auto main() -> void {
   init();
+
+  end();
 }
